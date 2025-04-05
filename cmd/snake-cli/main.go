df23ccc3e247e4587e6b5422dbbde8cd80c02785
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-04-05 16:44:30 -0400

    feat: basic bubbletea app

--- cmd/snake-cli/main.go
+++ cmd/snake-cli/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type app struct{}
+
+func (a app) Init() tea.Cmd {
+	return nil
+}
+
+func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		if keyMsg.Type == tea.KeyCtrlC {
+			return a, tea.Quit
+		}
+	}
+	return a, nil
+}
+
+func (a app) View() string {
+	return "Hello world"
+}
 
 func main() {
-	fmt.Printf("Hello world\n")
+	prog := tea.NewProgram(app{})
+	_, err := prog.Run()
+	if err != nil {
+		panic(err)
+	}
 }
