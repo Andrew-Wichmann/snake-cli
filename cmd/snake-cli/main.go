48a1ba156ecf6de18f79de8c9e97ae7a2dc95a90
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-04-05 20:20:28 -0400

    feat: basic vim-motions based controls

--- cmd/snake-cli/main.go
+++ cmd/snake-cli/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"image"
 	"image/png"
 	"os"
@@ -16,34 +18,60 @@ func newApp() app {
 	if err != nil {
 		panic(err)
 	}
-	image, err := png.Decode(file)
+	grid, err := png.Decode(file)
 	if err != nil {
 		panic(err)
 	}
-	app.img = image
+	app.image = grid
+	app.pos = image.Point{app.image.Bounds().Max.X / 2, app.image.Bounds().Max.Y / 2}
 	app.Render()
 	return app
 }
 
 func (a *app) Render() {
-	ctx := gg.NewContextForImage(a.img)
-	rect := a.img.Bounds()
+	ctx := gg.NewContextForImage(a.image)
 	ctx.SetRGBA255(255, 0, 0, 255)
-	ctx.DrawCircle(float64(rect.Max.X)/2, float64(rect.Max.Y)/2, 50)
+	ctx.DrawCircle(float64(a.pos.X), float64(a.pos.Y), 10)
 	ctx.Fill()
-	a.img = ctx.Image()
+	a.image = ctx.Image()
 	converter := ascii.NewImageConverter()
-	asciiString := converter.Image2ASCIIString(a.img, &ascii.DefaultOptions)
+	asciiString := converter.Image2ASCIIString(a.image, &ascii.DefaultOptions)
 	a.imageBuf = asciiString
 }
 
 type app struct {
-	img      image.Image
+	image    image.Image
 	imageBuf string
+	pos      image.Point
 }
 
 func (a app) Init() tea.Cmd {
 	return nil
+}
+
+type Direction int
+
+const (
+	UP    Direction = 0
+	DOWN  Direction = 1
+	LEFT  Direction = 2
+	RIGHT Direction = 3
+)
+
+func UpCmd() tea.Msg {
+	return UP
+}
+
+func DownCmd() tea.Msg {
+	return DOWN
+}
+
+func LeftCmd() tea.Msg {
+	return LEFT
+}
+
+func RightCmd() tea.Msg {
+	return RIGHT
 }
 
 func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -51,6 +79,30 @@ func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if keyMsg.Type == tea.KeyCtrlC {
 			return a, tea.Quit
 		}
+		switch keyMsg.String() {
+		case "k":
+			return a, UpCmd
+		case "j":
+			return a, DownCmd
+		case "l":
+			return a, RightCmd
+		case "h":
+			return a, LeftCmd
+		}
+	}
+	if direction, ok := msg.(Direction); ok {
+		if direction == UP {
+			a.pos.Y += -1
+		} else if direction == DOWN {
+			a.pos.Y += 1
+		} else if direction == LEFT {
+			a.pos.X += -1
+		} else if direction == RIGHT {
+			a.pos.X += 1
+		} else {
+			panic(errors.New(fmt.Sprintf("Unknown direction %d", direction)))
+		}
+		a.Render()
 	}
 	return a, nil
 }
