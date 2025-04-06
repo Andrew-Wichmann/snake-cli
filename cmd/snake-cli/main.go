c041b6a1348ef1759877e003246ccb71d17f4146
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-04-05 20:44:49 -0400

    feat: remove "grid" and make full screen

--- cmd/snake-cli/main.go
+++ cmd/snake-cli/main.go
@@ -4,34 +4,25 @@ import (
 	"errors"
 	"fmt"
 	"image"
-	"image/png"
-	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/fogleman/gg"
 	ascii "github.com/qeesung/image2ascii/convert"
 )
 
-func newApp() app {
-	var app app
-	file, err := os.Open("silly-grid.png")
-	if err != nil {
-		panic(err)
-	}
-	grid, err := png.Decode(file)
-	if err != nil {
-		panic(err)
-	}
-	app.image = grid
-	app.pos = image.Point{app.image.Bounds().Max.X / 2, app.image.Bounds().Max.Y / 2}
-	app.Render()
-	return app
+func NewGame() app {
+	var a app
+	ctx := gg.NewContext(width, height*2) // Not sure why I have to double the height...
+	a.image = ctx.Image()
+	a.pos = image.Point{a.image.Bounds().Max.X / 2, a.image.Bounds().Max.Y / 2}
+	a.Render()
+	return a
 }
 
 func (a *app) Render() {
 	ctx := gg.NewContextForImage(a.image)
 	ctx.SetRGBA255(255, 0, 0, 255)
-	ctx.DrawCircle(float64(a.pos.X), float64(a.pos.Y), 10)
+	ctx.DrawCircle(float64(a.pos.X), float64(a.pos.Y), 1)
 	ctx.Fill()
 	a.image = ctx.Image()
 	converter := ascii.NewImageConverter()
@@ -39,7 +30,19 @@ func (a *app) Render() {
 	a.imageBuf = asciiString
 }
 
+type State int
+
+var height int
+var width int
+
+const (
+	INIT    State = 0
+	RUNNING State = 1
+	PAUSED  State = 2 // maybe useful later (shrug)
+)
+
 type app struct {
+	state    State
 	image    image.Image
 	imageBuf string
 	pos      image.Point
@@ -75,6 +78,12 @@ func RightCmd() tea.Msg {
 }
 
 func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if windowSizeMsg, ok := msg.(tea.WindowSizeMsg); ok {
+		height = windowSizeMsg.Height
+		width = windowSizeMsg.Width
+		app := NewGame()
+		return app, nil
+	}
 	if keyMsg, ok := msg.(tea.KeyMsg); ok {
 		if keyMsg.Type == tea.KeyCtrlC {
 			return a, tea.Quit
@@ -88,6 +97,9 @@ func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return a, RightCmd
 		case "h":
 			return a, LeftCmd
+		case "n":
+			app := NewGame()
+			return app, nil
 		}
 	}
 	if direction, ok := msg.(Direction); ok {
@@ -112,8 +124,7 @@ func (a app) View() string {
 }
 
 func main() {
-	app := newApp()
-	prog := tea.NewProgram(app)
+	prog := tea.NewProgram(app{})
 	_, err := prog.Run()
 	if err != nil {
 		panic(err)
